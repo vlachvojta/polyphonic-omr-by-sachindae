037286b48029862354a3ed725f23e0fb3262fcd9
vlachvojta <vojtik.vlach@seznam.cz>
2023-03-15 16:38:02 +0100

    add more elements to delete from .musicxml file

--- label_gen/removecredits.py
+++ label_gen/removecredits.py
@@ -1,4 +1,4 @@
-# Script for removing <credit> elements from MusicXMLs
+# Script for removing elements (credits, lyrics, labels and dynamics) from MusicXMLs
 # run with python removecredits.py -input <input dir>
 
 import sys
@@ -17,12 +17,17 @@ def main():
 
     # Parse command line arguments for input directory
     parser = argparse.ArgumentParser()
-    parser.add_argument('-input', dest='input', type=str, required='-c' not in sys.argv, help='Path to the input directory with MusicXMLs.')
+    parser.add_argument('-input', dest='input', type=str,
+                        required='-c' not in sys.argv,
+                        help='Path to the input directory with MusicXMLs.')
     args = parser.parse_args()
 
     # Go through all files in input directory
-    for file_name in os.listdir(args.input):
-     
+    files = os.listdir(args.input)
+    print(f'Going through {len(files)} files, every dot is 200 files. ')
+    for i, file_name in enumerate(files):
+        if i % 200 == 0:
+            print('.', end='')
         # Make sure file is .musicxml
         if not file_name.endswith('.musicxml'):
             continue
@@ -43,6 +48,18 @@ def main():
         for elem in doc.xpath('//rights'):
             parent = elem.getparent()
             parent.remove(elem)
+        for elem in doc.xpath('//lyric'):
+            parent = elem.getparent()
+            parent.remove(elem)
+        for elem in doc.xpath('//part-name'):
+            elem.text = ''
+        for elem in doc.xpath('//instrument-name'):
+            elem.text = ''
+        for elem in doc.xpath('//part-abbreviation'):
+            elem.text = ''
+        for elem in doc.xpath('//direction'):
+            parent = elem.getparent()
+            parent.remove(elem)
 
         # Write to same file with next XML
         f = open(input_file, 'wb')
@@ -50,6 +67,7 @@ def main():
         f.close()
         num_files += 1
 
+    print('')
     print('Total files:',num_files)
 
 
