958721a13d3e36ce1b0be71f2c45a16dfa9fd77f
vlachvojta <vojtik.vlach@seznam.cz>
2023-03-21 12:01:28 +0100

    nicer printing, delete comments

--- label_gen/genlabels.py
+++ label_gen/genlabels.py
@@ -35,8 +35,8 @@ def parseargs():
 
 def save_labels(output_file, labels_db) -> None:
     """Save labels form list of tuples to a file."""
-    print(f'Saving to {output_file}...')
     output_lines = [f'{file} "{labels}"' for file, labels in labels_db]
+    new_lines_len = len(output_lines)
 
     if os.path.exists(output_file):
         with open(output_file, 'r', encoding='utf-8') as f:
@@ -45,16 +45,15 @@ def save_labels(output_file, labels_db) -> None:
 
             # filter out empty lines
             orig_lines = list(filter(None, orig_lines))
-            print(f'There is already {len(orig_lines)} lines in the output file. Concatenating.')
 
         output_lines = sorted(set(output_lines + orig_lines))
-        print(f'{len(output_lines)} after concatenating.')
+        print(f'Saving new {new_lines_len} label lines to original {len(orig_lines)}, '
+              f'new total: {len(output_lines)}')
 
     output = '\n'.join(sorted(output_lines)) + '\n'
 
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(output)
-    print(f'Labels saved to {output_file}')
 
 
 def get_files(folder):
@@ -74,20 +73,11 @@ def main():
 
     start = time.time()
 
-    # if args.verbose:
-    #     logging.basicConfig(level=logging.DEBUG, format='[%(levelname)-s]\t- %(message)s')
-    # else:
-    #     logging.basicConfig(level=logging.INFO,format='[%(levelname)-s]\t- %(message)s')
-
     labels_all = []
-    db_file_name = 'generated_labels.semantic'
     input_files = get_files(args.input_folder)
 
     # Go through all inputs generating output sequences
     for _, file_name in enumerate(input_files):
-
-        # output_path = os.path.join(args.output_folder, ''.join(file_name.split('.')[:-1]))
-
         # Create a MusicXML object for generating sequences
         musicxml_obj = MusicXML(input_file=file_name)
         # musicxml_obj = MusicXML(input_file=input_path, output_file=output_path)
@@ -98,10 +88,10 @@ def main():
         # except UnicodeDecodeError: # Ignore bad MusicXML
         #     pass
 
+    print('--------------------------------------')
+    db_file_name = 'generated_labels.semantic'
     db_file_name_path = os.path.join(args.output_folder, db_file_name)
     save_labels(db_file_name_path, labels_all)
-
-    print('--------------------------------------')
     print('Results:')
     print(f'From {len(input_files)} input files')
     print(f'\tgot {len(labels_all)} label lines')
