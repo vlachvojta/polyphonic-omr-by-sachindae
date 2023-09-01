040cd2b68dc3dacf35ec6e02e6548eab79eeefec
vlachvojta <vojtik.vlach@seznam.cz>
2023-09-01 14:40:41 +0200

    Mege `internal_symbols.py` to `symbol.py`

--- reverse_converter/symbol.py
+++ reverse_converter/symbol.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.10
-"""Script containing Symbol class capable of representing Semantic label as other formats.
+"""Script containing classes for internal representation of Semantic labels.
+Symbol class is default symbol for parsing and returning music21 representation.
+Other classes are internal representations of different symbols.
 
 Primarily used for compatibility with music21 library.
 
@@ -7,12 +9,12 @@ Author: Vojtěch Vlach
 Contact: xvlach22@vutbr.cz
 """
 
+from __future__ import annotations
 import logging
 from enum import Enum
 import re
 
 import music21 as music
-from internal_symbols import Note, MultiRest, Tie
 from common_rev_conv import label_to_length
 from common_rev_conv import AlteredPitches
 
@@ -145,7 +147,7 @@ class Symbol:
             return return_default_multirest()
 
     @staticmethod
-    def note_to_symbol(note, gracenote: bool = False) -> Note:  # -> Note:
+    def note_to_symbol(note, gracenote: bool = False) -> Note:
         """Converts one note label to internal note format.
 
         Args:
@@ -238,3 +240,67 @@ class Symbol:
             fermata.type = 'upright'
 
         return label, fermata
+
+
+class Note:
+    """Represents one note in a label group.
+
+    In the order which semantic labels are represented, the real height of note depends on key signature
+    of current measure. This class is used as an internal representation of a note before knowing its real height.
+    Real height is then stored directly in `self.note` as music.note.Note object.
+    """
+
+    def __init__(self, duration: music.duration.Duration, height: str,
+                 fermata: music.expressions.Fermata = None, gracenote: bool = False):
+        self.duration = duration
+        self.height = height
+        self.fermata = fermata
+        self.note = None
+        self.gracenote = gracenote
+        self.note_ready = False
+
+    def get_real_height(self, altered_pitches: AlteredPitches) -> music.note.Note | None:
+        """Returns the real height of the note.
+
+        Args:
+            key signature of current measure
+        Returns:
+             Final music.note.Note object representing the real height and other info.
+        """
+        if self.note_ready:
+            return self.note
+
+        # pitches = [pitch.name[0] for pitch in key.alteredPitches]
+
+        if not self.height[1:-1]:
+            # Note has no accidental on its own and takes accidental of the altered pitches.
+            note_str = self.height[0] + altered_pitches[self.height[0]] + self.height[-1]
+            self.note = music.note.Note(note_str, duration=self.duration)
+        else:
+            # Note has accidental which directly tells real note height.
+            note_str = self.height[0] + self.height[1:-1].replace('b', '-') + self.height[-1]
+            self.note = music.note.Note(note_str, duration=self.duration)
+            # Note sets new altered pitch for future notes.
+            altered_pitches[self.height[0]] = note_str[1:-1]
+
+        if self.gracenote:
+            self.note = self.note.getGrace()
+        self.note_ready = True
+        return self.note
+
+    def __str__(self):
+        return f'note {self.height} {self.duration}'
+
+
+class MultiRest:
+    """Represents one multi rest in a label group."""
+
+    def __init__(self, duration: int = 0):
+        self.duration = duration
+
+
+class Tie:
+    """Represents one tie in a label group."""
+
+    def __str__(self):
+        return 'tie'
